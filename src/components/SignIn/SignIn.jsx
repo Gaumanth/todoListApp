b992e753e5bc99cb9abc328f901e7adc881dd51b
Gaumanth Maliyal <gaumanthmaliyal1235@gmail.com>
2023-03-29 00:04:05 +0530

    Login page Completed on login branch

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,40 +1,31 @@
 import React from 'react'
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import logo from "../../assets/logo2.jpg"
-import backImg from "../../assets/background.jpg"
+import BackImg from "../../assets/background.jpg"
+import formImg from "../../assets/form-background.jpg"
 import "./SignIn.scss"
 const SignIn = () => {
   return (
-    <div className="signIn" style={{backgroundImage : `url(${backImg})`}}>
-<div className="container" >
-        <div className='form-container' >
-        <Form className='form'>
-    <Form.Group className="mb-3" controlId="formBasicEmail">
-      <Form.Label>Email address</Form.Label>
-      <Form.Control type="email" placeholder="Enter email" />
-      <Form.Text className="text-muted">
-        We'll never share your email with anyone else.
-      </Form.Text>
-    </Form.Group>
-
-    <Form.Group className="mb-3" controlId="formBasicPassword">
-      <Form.Label>Password</Form.Label>
-      <Form.Control type="password" placeholder="Password" />
-    </Form.Group>
-    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-      <Form.Check type="checkbox" label="Check me out" />
-    </Form.Group>
-    <Button variant="primary" type="submit">
-      Submit
-    </Button>
-  </Form>
-        </div>
-
+    <div className='form' style={{backgroundImage : `url(${BackImg})`}}>
+          <div className="container" style={{backgroundImage : `url(${formImg})`}}>
+            <div className="form-content">
+                <div className="top">
+                  <h1 className='heading1'>Welcome To Todo App</h1>
+                  <h2 className='heading2' >Login</h2>
+                </div>
+                <div className="middle">
+                  <input type="email" placeholder='Email Address' />
+                  <input type="password" placeholder='Password' />
+                    <div className="btns">
+                      <button className='login-btn'>LOGIN</button>
+                      <div className='fp'>Forgot Password?</div>
+                    </div>
+                </div>
+                <div className="bottom">
+                  <div className="nau">not a user?</div>
+                  <button className='signUp-btn'>SIGN UP</button>
+                </div>
+            </div>
+          </div>
     </div>
-    </div>
-    
-    
   )
 }
 
